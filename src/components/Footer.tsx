37ebb36a8902a08af5527af7153b052960be175a
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-07-02 19:33:09 -0600

    Add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="p-2 bg-purple-900 text-center text-xl text-green-500">
+      <p>
+        Made with <span className="text-orange-700">❤</span> by{" "}
+        <a
+          href="https://github.com/Marvin-Figueroa"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-indigo-500"
+        >
+          Marvin Figueroa
+        </a>
+        &nbsp; &copy; 2024
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
